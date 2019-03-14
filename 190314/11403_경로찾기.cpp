eb09a0ace70a78f0106a226060f8dcf250482ade
홍유진 <hyj5419@hanyang.ac.kr>
2019-03-14 20:48:58 +0900

    190314

---
+++ 190314/11403_경로찾기.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<iostream>
+#include<queue>
+#include<vector>
+#include<algorithm>
+#define pii pair<int, int>
+using namespace std;
+int arr[105][105];
+int ans[105][105];
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			scanf("%d", &arr[i][j]);
+		}
+	}//인접행렬
+
+	for (int k = 0; k < n; k++) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (arr[i][k]==1 && arr[k][j]==1) {
+					arr[i][j] = 1;
+				}
+			}
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+
+}
